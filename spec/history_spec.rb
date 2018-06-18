e8cd59811935ad691eb271a21d49508fd14919c5
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-18 16:14:48 +0100

    Change date format in tests for consistency

--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -11,15 +11,15 @@ describe History do
 
   describe '#add_deposit' do
     it 'should add transaction info to history data' do
-      history.add_deposit(1000, 0, '18 June 2018')
-      expect(history.data).to eq([{date: '18 June 2018', credit: 1000, balance: 1000}])
+      history.add_deposit(1000, 0, '18/06/2018')
+      expect(history.data).to eq([{date: '18/06/2018', credit: 1000, balance: 1000}])
     end
   end
 
   describe '#add_withdrawal' do
     it 'should add transaction info to history data' do
-      history.add_withdrawal(300, 1000, '18 June 2018')
-      expect(history.data).to eq([{date: '18 June 2018', debit: 300, balance: 700}])
+      history.add_withdrawal(300, 1000, '18/06/2018')
+      expect(history.data).to eq([{date: '', debit: 300, balance: 700}])
     end
   end
 
@@ -29,8 +29,8 @@ describe History do
     end
 
     it 'display transaction history after deposit' do
-      history.add_deposit(1000, 0, '18 June 2018')
-      expect{history.display}.to output("date || credit || debit || balance\n18 June 2018 || 1000 ||  || 1000\n").to_stdout
+      history.add_deposit(1000, 0, '18/06/2018')
+      expect{history.display}.to output("date || credit || debit || balance\n18/06/2018 || 1000 ||  || 1000\n").to_stdout
     end
   end
 end
