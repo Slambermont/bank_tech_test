ee8e1594b3ad0f528588a5cdc64e19afb657d836
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-19 12:55:12 +0100

    Fix Rubocop offense

--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -44,8 +44,7 @@ describe History do
       history.add_deposit(2000, 1000)
       Timecop.freeze(Time.local(2012, 01, 14))
       history.add_withdrawal(500, 3000)
-      expect { history.display }.to output(
-        "date || credit || debit || balance\n14/01/2012 ||  || 500.00 || 2500.00\n13/01/2012 || 2000.00 ||  || 3000.00\n10/01/2012 || 1000.00 ||  || 1000.00\n").to_stdout
+      expect { history.display }.to output("date || credit || debit || balance\n14/01/2012 ||  || 500.00 || 2500.00\n13/01/2012 || 2000.00 ||  || 3000.00\n10/01/2012 || 1000.00 ||  || 1000.00\n").to_stdout
     end
   end
 end
